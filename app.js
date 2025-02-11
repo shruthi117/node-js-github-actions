e10576d49938a7109b03e6c25347d5153ddd6497
shruthi117 <124680890+shruthi117@users.noreply.github.com>
2025-02-11 12:04:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Welcome to Demo on GitHub Actions-Devops</h1>`);
+  res.send(`<h1>Welcome to Demo on GitHub Actions-Devops.</h1>`);
 })
 
 app.get('/products', (req, res) => {
